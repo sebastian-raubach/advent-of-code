e92df8c8648622329b80d244f8b99289eb4b323a
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2020-12-01 06:56:03 +0000

    - ADD: Added solution for day 1 part 1 and 2.

--- src/components/Day1.vue
+++ src/components/Day1.vue
@@ -15,12 +15,46 @@ export default {
       solutions: {
         partOne: null,
         partTwo: null
-      }
+      },
+      numbers: []
     }
   },
   methods: {
     onInputChanged: function (input) {
-      // TODO: Implement on day 1
+      this.numbers = input.map(i => +i)
+      this.solvePartOne()
+      this.solvePartTwo()
+    },
+    solvePartOne: function () {
+      // Iterate through
+      for (let i = 0; i < this.numbers.length; i++) {
+        // Start second loop only from i
+        for (let j = i; j < this.numbers.length; j++) {
+          if (this.numbers[i] + this.numbers[j] === 2020) {
+            this.solutions.partOne = this.numbers[i] * this.numbers[j]
+            return
+          }
+        }
+      }
+
+      this.solutions.partOne = 'No solution found'
+    },
+    solvePartTwo: function (input) {
+      // Iterate through
+      for (let i = 0; i < this.numbers.length; i++) {
+        // Start second loop only from i
+        for (let j = i; j < this.numbers.length; j++) {
+          // Start the third loop only from j
+          for (let k = j; k < this.numbers.length; k++) {
+            if (this.numbers[i] + this.numbers[j] + this.numbers[k] === 2020) {
+              this.solutions.partTwo = this.numbers[i] * this.numbers[j] * this.numbers[k]
+              return
+            }
+          }
+        }
+      }
+
+      this.solutions.partTwo = 'No solution found'
     }
   }
 }
