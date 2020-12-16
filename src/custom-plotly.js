32a45a14360516b4f8eed7f62d3f9ea86a11430d
Sebastian Raubach <=>
2020-12-16 17:16:37 +0000

    - ADD: Added plotly line chart to stats page.

--- src/custom-plotly.js
+++ src/custom-plotly.js
@@ -1,3 +1,8 @@
 const Plotly = require('plotly.js/lib/core')
 
+// Only register the chart types we're actually using to reduce the final bundle size
+Plotly.register([
+  require('plotly.js/lib/scatter')
+])
+
 module.exports = Plotly
--- src/views/JsonStats.vue
+++ src/views/JsonStats.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
+    <h1>Statistics</h1>
+    <h2>Input</h2>
+    <p>Paste your private leaderboard API JSON into this text area to see some stats.</p>
     <b-form @submit.prevent>
       <b-form-group label="JSON API input" label-for="json" description="The JSON API output of a leaderboard.">
         <b-form-textarea v-model="input" rows=5 id="json" />
       </b-form-group>
     </b-form>
 
-    <b-table table-class="star-table mt-3" :items="json" :fields="fields" stacked="sm" sort-by="local_score" :sort-desc="true" v-if="json">
-      <template #cell(days)="data">
-        <template v-for="day in tasks" >
-          <span :key="`${data.id}-${day}`" v-b-tooltip.body.html="getTooltip(day, data.item.completion_day_level[day])" class="star star-gold" v-if="data.item.completion_day_level[day] && data.item.completion_day_level[day][2]">★</span>
-          <span :key="`${data.id}-${day}`" v-b-tooltip.body.html="getTooltip(day, data.item.completion_day_level[day])" class="star star-silver" v-else-if="data.item.completion_day_level[day] && data.item.completion_day_level[day][1]">★</span>
-          <span :key="`${data.id}-${day}`" class="star star-none" v-else>☆</span>
+    <template v-if="json">
+      <h2>Stats overview</h2>
+      <p>Hover over the stars to see the timestamps for part 1 and part 2 as well as the user's ranking for the day.</p>
+      <b-table table-class="star-table mt-3" :items="json" :fields="fields" stacked="sm" sort-by="local_score" :sort-desc="true">
+        <template #cell(days)="data">
+          <template v-for="day in tasks" >
+            <span :key="`${data.id}-${day}`" v-b-tooltip.body.html="getTooltip(day, data.item.completion_day_level[day])" class="star star-gold" v-if="data.item.completion_day_level[day] && data.item.completion_day_level[day][2]">★</span>
+            <span :key="`${data.id}-${day}`" v-b-tooltip.body.html="getTooltip(day, data.item.completion_day_level[day])" class="star star-silver" v-else-if="data.item.completion_day_level[day] && data.item.completion_day_level[day][1]">★</span>
+            <span :key="`${data.id}-${day}`" class="star star-none" v-else>☆</span>
+          </template>
         </template>
-      </template>
-    </b-table>
+      </b-table>
+    </template>
+
+    <template v-if="userTraces">
+      <h2>Points over time</h2>
+      <div id="timeline-chart" class="my-3" />
+    </template>
   </div>
 </template>
 
@@ -49,10 +61,42 @@ export default {
         try {
           const parsed = JSON.parse(this.input)
 
-          return Object.keys(parsed.members).map(m => parsed.members[m])
+          return Object.keys(parsed.members).map(m => parsed.members[m]).sort((a, b) => a.local_score - b.local_score)
         } catch (err) {
           return null
         }
+      } else {
+        return null
+      }
+    },
+    userTraces: function () {
+      if (this.json) {
+        return this.json.map(user => {
+          let cumulative = 0
+          const values = []
+          values.push(cumulative)
+          for (let day = 0; day < this.tasks.length; day++) {
+            if (user.completion_day_level[day + 1]) {
+              const partOneTimestamp = user.completion_day_level[day + 1][1] ? +user.completion_day_level[day + 1][1].get_star_ts : null
+              const partTwoTimestamp = user.completion_day_level[day + 1][2] ? +user.completion_day_level[day + 1][2].get_star_ts : null
+
+              if (partOneTimestamp) {
+                const partOneIndex = this.sortedUsersPerDay[day].partOne.indexOf(partOneTimestamp)
+                cumulative += this.json.length - partOneIndex
+              }
+              if (partTwoTimestamp) {
+                const partTwoIndex = this.sortedUsersPerDay[day].partTwo.indexOf(partTwoTimestamp)
+                cumulative += this.json.length - partTwoIndex
+              }
+
+              if (partOneTimestamp || partTwoTimestamp) {
+                values.push(cumulative)
+              }
+            }
+          }
+
+          return values
+        })
       } else {
         return null
       }
@@ -88,7 +132,39 @@ export default {
       }
     }
   },
+  watch: {
+    userTraces: function () {
+      this.$nextTick(() => this.updateChart())
+    }
+  },
   methods: {
+    updateChart: function () {
+      this.$plotly.purge('timeline-chart')
+
+      const traces = this.userTraces.map((ut, ui) => {
+        return {
+          x: ut.map((v, i) => i),
+          y: ut,
+          name: this.json[ui].name,
+          type: 'scatter'
+        }
+      })
+
+      this.$plotly.newPlot('timeline-chart', traces, {
+        height: 350 + this.userTraces.length / 5 * 10,
+        margin: { t: 10, b: 10, l: 50, r: 0 },
+        yaxis: {
+          title: 'Points'
+        },
+        legend: {
+          orientation: 'h',
+          traceorder: 'reversed'
+        }
+      }, {
+        responsive: true,
+        displaylogo: false
+      })
+    },
     getTooltip: function (day, data) {
       let result = `<p>Day ${day}</p>`
       if (data[1] && data[1].get_star_ts) {
