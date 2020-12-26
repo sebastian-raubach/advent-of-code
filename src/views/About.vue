047564c4d6d17298a3116122cb3a560f70d82276
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2020-12-26 16:05:23 +0000

    - FIX: Fixed repo link on the about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h1>About</h1>
     <p>This is my <a href="https://adventofcode.com/">Advent of Code</a> solution for 2020 as well as some previous years that I'll add as I finish them.</p>
     <p>I decided to switch from Java (2019) to Javascript this time around to make things a bit more intersting.</p>
-    <p>The source code for this website is hosted on GitHub: <a href="https://github.com/sebastian-raubach/advent-of-code-2020">https://github.com/sebastian-raubach/advent-of-code-2020</a></p>
+    <p>The source code for this website is hosted on GitHub: <a href="https://github.com/sebastian-raubach/advent-of-code">https://github.com/sebastian-raubach/advent-of-code</a></p>
   </div>
 </template>
 
